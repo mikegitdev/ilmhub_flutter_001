ce49b48553a13f36b871da2733009651998a5d08
@Mukhriddin <ergashaliev.m@jafton.com>
2022-12-02 21:23:45 +0500

    10 -dars qo'shildi

---
+++ lib/10_dars.dart
@@ -0,0 +1,20 @@
+void main() {
+  var javob = qoshish(2, 3);
+  print(javob);
+}
+
+void salomBeruvchi() {
+  print("Salom  Salom Salom Salom");
+}
+
+// String salomBerdi() {
+//   return "Salom  Salom Salom Salom";
+// }
+
+int qoshish(int num1, int num2) {
+  return num1 + num2;
+}
+
+// alikOldi() {
+//   return null;
+// }
