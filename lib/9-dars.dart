b706d00de758e504f72871c13fa9b08d4a262c93
@Mukhriddin <ergashaliev.m@jafton.com>
2022-12-02 17:02:40 +0500

    dars

---
+++ lib/9-dars.dart
@@ -0,0 +1,82 @@
+import 'dart:collection';
+
+main() {
+//   HashMap hashMap = new HashMap<int, String>();
+//   LinkedHashMap linkedHashMap = new LinkedHashMap<int, String>();
+//   SplayTreeMap treeMap = new SplayTreeMap<int, String>();
+
+//   Map map = Map<int, String>();
+//   if (map is LinkedHashMap) {
+//     print("This is a LinkedHashMap.");
+//   }
+
+//   HashMap map1 = HashMap();
+//   LinkedHashMap map2 = LinkedHashMap();
+//   SplayTreeMap map3 = SplayTreeMap();
+
+//   Map<String, int> map11 = {'zero': 0, 'one': 1, 'two': 2};
+//   print(map11);
+
+// // not specify key-value type
+//   Map map22 = {'zero': 0, 'I': 'one', 10: 'X'};
+//   print(map22);
+
+// +++++++++++++++++++++++
+
+  // var ozgaruvchi = true;
+  // var myMap1 = {1: 'kotlin', 2: 'dart', if (ozgaruvchi) 3: 'flutter'};
+  // var myMap = {1: 'kotlin', 2: 'dart'};
+  // if (ozgaruvchi) {
+  //   myMap[3] = 'flutter';
+  // }
+  // print(myMap);
+
+  // var squareMap = {for (var i = 1; i <= 5; i++) i: i * i};
+  // print(squareMap);
+//   var tringList = ['kotlin', 'dart', 'flutter', 'react'];
+//   var i = 0;
+//   var data = {
+//     for (var s in tringList)
+//       if (s.length > 5) i++: s
+//   };
+// {0: kotlin, 1: flutter}
+
+  // var map = {1: 'one', 3: 'two'};
+
+  // map[2] = 'three';
+  // print(map);
+
+  // final hashmap = HashMap.of(map);
+
+  // print(hashmap);
+
+  // Map map = {"d": 1, "b": 2, "c": 3};
+
+  // final newMap = HashMap.of(map);
+
+  // (true || false) ? print("Hello") : print("Good Bye");
+
+  // if (newMap is HashMap) {
+  //   print('Yes it is');
+  // } else {
+  //   print('No it is not');
+  // }
+
+  // newMap["a"] = 4;
+  // LinkedHashMap
+  // HashMap
+  //SplayTreeMap;
+
+  // print(newMap);
+
+  // var myList = [1, 'sovun', 'sovun', 'sovun', 2, 2, 2, 2, 2, 3, 4];
+  // var myProds = {};
+
+  // for (final item in myList) {
+  //   if (myProds.contains(item)) {
+  //     print("Yes");
+  //   }
+  //   myProds.add(item);
+  // }
+  // print(myProds);
+}
