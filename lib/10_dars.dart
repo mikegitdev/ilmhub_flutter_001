e851e8d67ba7f948891019d4baf7e2e78e3c7e38
Abdujabbor17 <jabborprimqulov@gmail.com>
2022-12-03 09:38:22 +0500

    10-dars davomi

--- lib/10_dars.dart
+++ lib/10_dars.dart
@@ -1,6 +1,51 @@
-void main() {
-  var javob = qoshish(2, 3);
-  print(javob);
+
+void main(){
+ // String nickname = 'Abror';
+  //hello('John', 'Carter');
+  // print(stringLength('Abror'));
+  // print("abror".length);
+  int number = 6;
+  print('$number ning kvadrati ${intSquare(number)}ga teng');
+
+  List<int> list = [1,4,5,7,9,3,12,1,5,6];
+  var max = maxElement(list);
+  var listMaxMin = maxMinElement(list);
+  print(listMaxMin);
+}
+
+void hello(String name , String surname , ){
+  //var letter = name[0];
+  print('Hello $name $surname');
+}
+
+int intSquare(int num){
+  return num * num;
+}
+
+int stringLength(String name){
+  return name.length;
+}
+int maxElement(List<int> list){
+   int max = list[0] ;
+  for(int item in list){
+    if(item > max){
+      max  = item;
+    }
+  }
+  return max;
+}
+List<int> maxMinElement(List<int> list){
+   int max = list[0] ;
+   int min = list[0] ;
+  for(int item in list){
+    if(item > max){
+      max  = item;
+    }
+     if(item < min){
+      min  = item;
+    }
+  }
+  return [max, min];
 }
 
 void salomBeruvchi() {
@@ -18,3 +63,5 @@ int qoshish(int num1, int num2) {
 // alikOldi() {
 //   return null;
 // }
+
+
